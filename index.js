fd5b182c75b95d735bf6c06c8d1ceced5c303fa7
Kelvin Machira <machirakelvin81@gmail.com>
2024-03-24 10:05:25 +0300

    Function lab continued complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+// Define the functions
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(symbol = "*") {
+    return function(adjective) {
+        return `You are ${symbol}, ${adjective},${symbol}!`;
+    };
+}
+function wrapAdjective(symbol = "*") {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    };
+}
+
+// Additional requirements
+const asteriskWrapper = wrapAdjective('*');
+const doublePipeWrapper = wrapAdjective('||');
+
+console.log(asteriskWrapper("amazing")); // You are *amazing*!
+console.log(doublePipeWrapper("incredible")); // You are ||incredible
